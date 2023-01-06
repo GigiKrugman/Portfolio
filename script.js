091527b7fd910e0548f217427cfb858b46d0d37a
Marcello Mulas <marcellomulas93@gmail.com>
2023-01-06 16:44:57 +0100

    Other style added

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let btnAbout = document.getElementById("btn-flat");
-let aboutSection = document.getElementById("about--section");
+let aboutSection = document.getElementById("about--me--paragraph");
 
 btnAbout.addEventListener("click", function () {
-  aboutSection.innerText = "Hello";
+  aboutSection.innerHTML = `<div class="card-product">
+      <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
+      <div class="card-product-infos">
+        <h2>Product name</h2>
+        <p>
+          Product description with <strong>relevant info</strong> only.
+        </p>
+      </div>
+    </div>`;
 });
