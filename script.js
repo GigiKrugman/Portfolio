e421d833efb90e14d3dacabda3eec86d2dc1ca82
Marcello Mulas <marcellomulas93@gmail.com>
2023-01-09 19:53:18 +0100

    card/Nav/About section

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ btnAbout.addEventListener("click", function () {
   aboutSection.innerHTML = `<div class="card-product">
       <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
       <div class="card-product-infos">
-        <h2>Product name</h2>
+        <h2>About me</h2>
         <p>
-          Product description with <strong>relevant info</strong> only.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         </p>
       </div>
     </div>`;
